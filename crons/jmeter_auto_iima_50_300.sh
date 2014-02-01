b34be71acc1046009da1601d8f901288a1268cae
Rajesh Soni <rajesh.soni@saviance.com>
2014-02-01 13:24:45 +0530

    cron changes

--- crons/jmeter_auto_iima_50_300.sh
+++ crons/jmeter_auto_iima_50_300.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 datecheck=$(date +"%Y%m%d%H%M")
+datefolder=$(date +"%d-%m-%Y")
 jmeter -n -t /opt/jmeter_script/iima_live16Jan2014_50users_300rampup.jmx -p /opt/jmeter_script/user.properties -l /tmp/iima_live16Jan2014_50users_300rampup$datecheck.jtl -j /var/www/html/iima_live16Jan2014_50users_300rampup$datecheck.log
 cd /tmp
 tar czf iima_live16Jan2014_50users_300rampup$datecheck.jtl.tgz iima_live16Jan2014_50users_300rampup$datecheck.jtl
-s3cmd put iima_live16Jan2014_50users_300rampup$datecheck.jtl.tgz s3://testup006/vib_performance/iima/
+s3cmd put iima_live16Jan2014_50users_300rampup$datecheck.jtl.tgz s3://testup006/vib_performance/iima/$datefolder/
--- crons/jmeter_auto_ops_50_30.sh
+++ crons/jmeter_auto_ops_50_30.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 datecheck=$(date +"%Y%m%d%H%M")
+datefolder=$(date +"%d-%m-%Y")
 jmeter -n -t /opt/jmeter_script/ops_demo_22jan2014_50users_30rampup.jmx -p /opt/jmeter_script/user.properties -l /tmp/ops_demo_22jan2014_50users_30rampup$datecheck.jtl -j /var/www/html/ops_demo_22jan2014_50users_30rampup$datecheck.log
 cd /tmp
 mv /opt/jmeter_script/ops_50users_30rampup.csv ./jmeter_script/ops_50users_30rampup$datecheck.csv
 tar czf ops_demo_22jan2014_50users_30rampup$datecheck.jtl.tgz ops_demo_22jan2014_50users_30rampup$datecheck.jtl
-s3cmd put ops_demo_22jan2014_50users_30rampup$datecheck.jtl.tgz s3://testup006/vib_performance/newiima/
+s3cmd put ops_demo_22jan2014_50users_30rampup$datecheck.jtl.tgz s3://testup006/vib_performance/newiima/$datefolder/

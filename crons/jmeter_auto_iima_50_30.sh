a6f8194dc6bef3925e8ab102b52691f2afdb9636
root <root@ip-10-99-46-65.ec2.internal>
2014-02-01 05:00:17 +0000

    cron changed

--- crons/jmeter_auto_iima_50_30.sh
+++ crons/jmeter_auto_iima_50_30.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 datecheck=$(date +"%Y%m%d%H%M")
+datefolder=$(date +"%d-%m-%Y")
 jmeter -n -t /opt/jmeter_script/iima_live16Jan2014_50users_30rampup.jmx -p /opt/jmeter_script/user.properties -l /tmp/iima_live16Jan2014_50users_30rampup$datecheck.jtl -j /var/www/html/iima_live16Jan2014_50users_30rampup$datecheck.log
 cd /tmp
 tar czf iima_live16Jan2014_50users_30rampup$datecheck.jtl.tgz iima_live16Jan2014_50users_30rampup$datecheck.jtl
-s3cmd put iima_live16Jan2014_50users_30rampup$datecheck.jtl.tgz s3://testup006/vib_performance/iima/
+s3cmd put iima_live16Jan2014_50users_30rampup$datecheck.jtl.tgz s3://testup006/vib_performance/iima/$datefolder/
